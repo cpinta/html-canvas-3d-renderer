f5d19c2347f6cf6baead14df0507e1ffac3a6bf2
cgpinta <pinta.christian@outlook.com>
2025-08-20 20:32:11 -0500

    moved 3D.tsx functions around

--- src/3D.tsx
+++ src/3D.tsx
@@ -1,35 +1,6 @@
 
 import { identityMatrix3, identityMatrix4, MMath } from "./Matrix"
 
-export class Vector3 {
-    x: number;
-    y: number;
-    z: number;
-
-    constructor(x : number, y : number, z : number){
-        this.x = x
-        this.y = y
-        this.z = z
-    }
-
-    translate(x:number, y:number, z:number){
-        let matrix = structuredClone(identityMatrix4)
-        matrix[0][3] = x;
-        matrix[1][3] = y;
-        matrix[2][3] = z;
-        let newVec = MMath.multiply(matrix, [[x],[y],[z]]);
-        if(newVec){
-            this.x = newVec[0][0]
-            this.y = newVec[1][0]
-            this.z = newVec[2][0]
-        }
-    }
-
-    toMatrix3(): number[][]{
-        return [[this.x],[this.y],[this.z]]
-    }
-}
-
 export class Object3D{
     name: string = ""
     objectMatrix: number[][] = structuredClone(identityMatrix3)
@@ -51,23 +22,6 @@ export class Object3D{
     
     wRotate(rotation: Vector3){
         this.worldMatrix = MMath.rotate(this.worldMatrix, rotation)
-    }
-}
-
-export class Face {
-    vertIndexes: number[]
-    largestVertIndex: number = -1
-
-    constructor(vertIndexes: number[]){
-        if(vertIndexes.length < 3){
-            throw new Error("Invalid face")
-        }
-        this.vertIndexes = vertIndexes
-        for(let i=0;i<vertIndexes.length;i++){
-            if(vertIndexes[i] > this.largestVertIndex){
-                this.largestVertIndex = vertIndexes[i]
-            }
-        }
     }
 }
 
@@ -95,5 +49,50 @@ export class Mesh {
         else{
             this.vert2faceMap.set(face.largestVertIndex, [this.faceArr.length - 1])
         }
+    }
+}
+
+export class Face {
+    vertIndexes: number[]
+    largestVertIndex: number = -1
+
+    constructor(vertIndexes: number[]){
+        if(vertIndexes.length < 3){
+            throw new Error("Invalid face")
+        }
+        this.vertIndexes = vertIndexes
+        for(let i=0;i<vertIndexes.length;i++){
+            if(vertIndexes[i] > this.largestVertIndex){
+                this.largestVertIndex = vertIndexes[i]
+            }
+        }
+    }
+}
+export class Vector3 {
+    x: number;
+    y: number;
+    z: number;
+
+    constructor(x : number, y : number, z : number){
+        this.x = x
+        this.y = y
+        this.z = z
+    }
+
+    translate(x:number, y:number, z:number){
+        let matrix = structuredClone(identityMatrix4)
+        matrix[0][3] = x;
+        matrix[1][3] = y;
+        matrix[2][3] = z;
+        let newVec = MMath.multiply(matrix, [[x],[y],[z]]);
+        if(newVec){
+            this.x = newVec[0][0]
+            this.y = newVec[1][0]
+            this.z = newVec[2][0]
+        }
+    }
+
+    toMatrix3(): number[][]{
+        return [[this.x],[this.y],[this.z]]
     }
 }
