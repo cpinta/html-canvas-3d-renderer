a905ff500f4d547258c6bebeaed4d95754a7b81c
cgpinta <pinta.christian@outlook.com>
2025-08-26 22:08:43 -0500

    added Plane and Line primitives

--- src/Primitives.tsx
+++ src/Primitives.tsx
@@ -1,13 +1,11 @@
 import { Color } from "./2D"
-import { Face, Mesh, Vector3 } from "./3D"
+import { Face, Mesh, Object3D, Vector3 } from "./3D"
 
-export class Cube extends Mesh {
+export class Cube extends Object3D {
 
     constructor(origin: Vector3, size: number, color: Color){
         let verts: Vector3[] = []
-        let edges: number[][] = []
-        let faces: number[][] = []
-        let faceObjs: Face[] = []
+        let faces: Face[] = []
 
         let halfSize: number = size/2
         verts.push(new Vector3(origin.x + halfSize, origin.y + halfSize, origin.z + halfSize))
@@ -20,14 +18,53 @@ export class Cube extends Mesh {
         verts.push(new Vector3(origin.x - halfSize, origin.y - halfSize, origin.z + halfSize))
         verts.push(new Vector3(origin.x - halfSize, origin.y - halfSize, origin.z - halfSize))
 
-        faceObjs.push(new Face([0,2,3,1], color))
-        faceObjs.push(new Face([0,2,6,4], color))
-        faceObjs.push(new Face([4,6,7,5], color))
-        faceObjs.push(new Face([5,7,3,1], color))
-        faceObjs.push(new Face([0,4,5,1], color))
-        faceObjs.push(new Face([2,6,7,3], color))
+        faces.push(new Face([0,2,3,1], color))
+        faces.push(new Face([0,2,6,4], color))
+        faces.push(new Face([4,6,7,5], color))
+        faces.push(new Face([5,7,3,1], color))
+        faces.push(new Face([0,4,5,1], color))
+        faces.push(new Face([2,6,7,3], color))
+
+        let mesh: Mesh = new Mesh(verts, faces)
         
+        super(mesh);
+    }
+}
+
+export class Plane extends Object3D {
+
+    constructor(origin: Vector3, size: number, color: Color){
+        let verts: Vector3[] = []
+        let faces: Face[] = []
+
+        let halfSize: number = size/2
+        verts.push(new Vector3(origin.x + halfSize, origin.y, origin.z + halfSize))
+        verts.push(new Vector3(origin.x + halfSize, origin.y, origin.z - halfSize))
+
+        verts.push(new Vector3(origin.x - halfSize, origin.y, origin.z - halfSize))
+        verts.push(new Vector3(origin.x - halfSize, origin.y, origin.z + halfSize))
+
+        faces.push(new Face([0,1,2,3], color, new Vector3(0,1,0)))
         
-        super(verts, faceObjs);
+        let mesh: Mesh = new Mesh(verts, faces)
+
+        super(mesh);
+    }
+}
+
+export class Line extends Object3D {
+
+    constructor(start:Vector3, end:Vector3, color: Color){
+        let verts: Vector3[] = []
+        let faces: Face[] = []
+
+        verts.push(start)
+        verts.push(end)
+
+        faces.push(new Face([0,1], color))
+        
+        let mesh: Mesh = new Mesh(verts, faces)
+
+        super(mesh);
     }
 }
